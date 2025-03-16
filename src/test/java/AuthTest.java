e7f6b7f95072b6dd6589b5bec00dbe8c9dafef07
888KEKC888 <sitneckov@mail.ru>
2025-03-16 21:42:49 +0300

    Initial commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,85 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        $("[data-test-id='login'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(registeredUser.getPassword());
+        $(".button").click();
+        $(byText("Личный кабинет")).shouldBe(visible);
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = DataGenerator.Registration.getUser("active");
+        $("[data-test-id='login'] .input__control").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(notRegisteredUser.getPassword());
+        $(".button").click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка!  Неверно указан логин или пароль"))
+                .shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = DataGenerator.Registration.getRegisteredUser("blocked");
+        $("[data-test-id='login'] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(blockedUser.getPassword());
+        $(".button").click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"))
+                .shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        var wrongLogin = DataGenerator.getRandomLogin();
+        $("[data-test-id='login'] .input__control").setValue(wrongLogin);
+        $("[data-test-id='password'] .input__control").setValue(registeredUser.getPassword());
+        $(".button").click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка!  Неверно указан логин или пароль"))
+                .shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        var wrongPassword = DataGenerator.getRandomPassword();
+        $("[data-test-id='login'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(wrongPassword);
+        $(".button").click();
+        $(withText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка!  Неверно указан логин или пароль"))
+                .shouldBe(visible);
+    }
+}
